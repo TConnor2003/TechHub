3640c4f4be48715ba186e3ab938ea3a8967e9e04
Tom-BPC <10656047@student.bpc.ac.uk>
2022-12-16 20:17:39 +0000

    add cardgenerator

--- js/randint.js
+++ js/randint.js
@@ -1,20 +1,19 @@
 //random integer between two given numbers animated
 function randint() {
-    var min = parseInt(document.getElementById("min").value);
-    var max = parseInt(document.getElementById("max").value);
-    var num = parseInt(document.getElementById("num").value);
-    if (num > 100) {
-        alert("Please enter a number less than 100");
-        return;
-    }
-    if (min > max) {
-        alert("Please enter a minimum number less than the maximum");
-        return;
-    }
-    var rand = [];
-    for (var i = 0; i < num; i++) {
-        rand[i] = Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    document.getElementById("output").innerHTML = rand;
+  var min = parseInt(document.getElementById("min").value);
+  var max = parseInt(document.getElementById("max").value);
+  var num = parseInt(document.getElementById("num").value);
+  if (num > 100) {
+    alert("Please enter a number less than 100");
+    return;
+  }
+  if (min > max) {
+    alert("Please enter a minimum number less than the maximum");
+    return;
+  }
+  var rand = [];
+  for (var i = 0; i < num; i++) {
+    rand[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  document.getElementById("output").innerHTML = rand;
 }
-
